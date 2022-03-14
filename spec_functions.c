d7db500945ec8c965270878b37e3a8255c4f5579
siham <phenissia2009@hotmail.fr>
2022-03-14 15:23:59 +0100

    handle spec_perc

--- spec_functions.c
+++ spec_functions.c
@@ -66,18 +66,18 @@ int	handle_spec_b(void *ptr)
 }
 
 /**
- * handle_spec_c - Function to get and output next char variable given to printf
+ * handle_spec_perc - Function to get and output next char variable given to printf
  * @ptr: Pointer to general data structure
  * Return: 0 if no error
  */
-int		handle_spec_p(void *ptr)
+int		handle_spec_perc(void *ptr)
 {
 	format_t	*data;
-	char		p[2];
+	char		perc[2];
 
 	data = (format_t*)ptr;
 	c[0] = va_arg(data->args, int);
 	c[1] = '\0';
-	write_buffer(p, 1, data);
+	write_buffer("%%", 1, data);
 	return (0);
 }
